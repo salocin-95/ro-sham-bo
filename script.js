d0555ede7d669d5d37453a966f1648772b3414b2
salocin-95 <renicolas7@gmail.com>
2023-03-30 20:51:56 -0300

    Add incomplete script.js

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-let globalScore = 0;
+// Keep track of the result. If at any time either the player or the computer have a higher score from 5 rounds show message with the result of the match.
+// Make it so the only option the player has is to put either rock, paper or scissors as value.
+// Once the game is over, replay and reset the rounds and the scores.
+// It would be easier to have buttons to force the player in to the options.
+// "Play Again?" button would reset the scores and call again game().
+// I need three do-while statements. One if player and the computer tie, one if the player loses and one if the player wins. 
+
+/*
+let rounds = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 function game() {
-    const messageTie = 'It`s a tie!'
+    const messageTie = 'It`s a tie!';
     const messageWin = 'You won!';
-    const messageLost = 'You lost!'
+    const messageLost = 'You lost!';
 
+    while (rounds < 5) {
+    rounds++
     function getComputerChoice() {
         let randomNumber = Math.floor((Math.random()*3))
         let computerChoice = '';
@@ -19,20 +31,31 @@ function game() {
 
 
     function playRound(playerSelection, computerSelection) {
+      /*  if (playerSelection != 'rock' || 'paper' || 'scissors') {
+            return
+        }
         if (playerSelection === computerSelection) {
-            return messageTie;
+            playerScore++
+            computerSelection++
+            console.log('You`ve tied this round!');
         } else if (playerSelection === 'rock' && computerSelection === 'paper' ) {
-            return messageLost;
+            computerScore++;
+            console.log('You`ve lost this round!');
         } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-            return messageLost;
+            computerScore++;
+            console.log('You`ve lost this round!');
         } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-            return messageLost;
+            computerScore++;
+            console.log('You`ve lost this round!');
         } else if (playerSelection === 'paper' && computerSelection === 'rock' ) {
-            return messageWin;
+            playerScore++;
+            console.log('You`ve won this round!');
         } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-            return messageWin;
+            playerScore++;
+            console.log('You`ve won this round!');
         } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-            return messageWin;
+            playerScore++;
+            console.log('You`ve won this round!');
         }
     
     }  
@@ -41,8 +64,93 @@ function game() {
     const computerSelection = getComputerChoice();
 
     playRound(playerSelection, computerSelection)
-    return globalScore++
-
+    }
 }
 
+
+do {
+    if (playerScore === computerScore) {
+        return messageTie;
+    } else if (playerScore < computerScore) {
+        return messageLost; 
+    } else if (playerScore > computerScore) {
+        return messageWin;
+    }
+} while (rounds < 5) {} 
+*/
+
+let rounds = 0;
+
+function game() {
+
+    let playerScore = 0;
+    let computerScore = 0;
+    const messageTie = 'It`s a tie!';
+    const messageWin = 'You won!';
+    const messageLost = 'You lost!';
+
+    while (rounds < 5) {
+    rounds++
+    function getComputerChoice() {
+        let randomNumber = Math.floor((Math.random()*3))
+        let computerChoice = '';
+        if (randomNumber === 0) {
+            return computerChoice = 'rock';
+        } else if (randomNumber === 1)  {
+            return computerChoice = 'paper';
+        } else if (randomNumber === 2) {
+            return computerChoice = 'scissors';
+        }
+    }
+
+
+    function playRound(playerSelection, computerSelection) {
+      /*  if (playerSelection != 'rock' || 'paper' || 'scissors') {
+            return
+        }*/
+        if (playerSelection === computerSelection) {
+            playerScore++
+            computerSelection++
+            console.log('You`ve tied this round!');
+        } else if (playerSelection === 'rock' && computerSelection === 'paper' ) {
+            computerScore++;
+            console.log('You`ve lost this round!');
+        } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+            computerScore++;
+            console.log('You`ve lost this round!');
+        } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+            computerScore++;
+            console.log('You`ve lost this round!');
+        } else if (playerSelection === 'paper' && computerSelection === 'rock' ) {
+            playerScore++;
+            console.log('You`ve won this round!');
+        } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+            playerScore++;
+            console.log('You`ve won this round!');
+        } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+            playerScore++;
+            console.log('You`ve won this round!');
+        }
+
+
+    }  
+
+    const playerSelection = prompt('¡Choose Rock, Paper or Scissors!').toLocaleLowerCase(); 
+    const computerSelection = getComputerChoice();
+
+    function scoreComparison(playerScore, computerScore) {
+        if (playerScore === computerScore) {
+            console.log(messageTie);
+        } else if (playerScore < computerScore) {
+            console.log(messageLost); 
+        } else if (playerScore > computerScore) {
+            console.log(messageWin);
+        }
+    }
+
+    playRound(playerSelection, computerSelection)
+    
+    }
+    scoreComparison(playerScore, computerScore);
+}
 
