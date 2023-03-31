8fa5de8fe043420fcc8f004cb87f77e06613854d
salocin-95 <renicolas7@gmail.com>
2023-03-31 07:14:13 -0300

    Modify script.js and update README.md

--- script.js
+++ script.js
@@ -5,80 +5,6 @@
 // "Play Again?" button would reset the scores and call again game().
 // I need three do-while statements. One if player and the computer tie, one if the player loses and one if the player wins. 
 
-/*
-let rounds = 0;
-let playerScore = 0;
-let computerScore = 0;
-
-function game() {
-    const messageTie = 'It`s a tie!';
-    const messageWin = 'You won!';
-    const messageLost = 'You lost!';
-
-    while (rounds < 5) {
-    rounds++
-    function getComputerChoice() {
-        let randomNumber = Math.floor((Math.random()*3))
-        let computerChoice = '';
-        if (randomNumber === 0) {
-            return computerChoice = 'rock';
-        } else if (randomNumber === 1)  {
-            return computerChoice = 'paper';
-        } else if (randomNumber === 2) {
-            return computerChoice = 'scissors';
-        }
-    }
-
-
-    function playRound(playerSelection, computerSelection) {
-      /*  if (playerSelection != 'rock' || 'paper' || 'scissors') {
-            return
-        }
-        if (playerSelection === computerSelection) {
-            playerScore++
-            computerSelection++
-            console.log('You`ve tied this round!');
-        } else if (playerSelection === 'rock' && computerSelection === 'paper' ) {
-            computerScore++;
-            console.log('You`ve lost this round!');
-        } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-            computerScore++;
-            console.log('You`ve lost this round!');
-        } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-            computerScore++;
-            console.log('You`ve lost this round!');
-        } else if (playerSelection === 'paper' && computerSelection === 'rock' ) {
-            playerScore++;
-            console.log('You`ve won this round!');
-        } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-            playerScore++;
-            console.log('You`ve won this round!');
-        } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-            playerScore++;
-            console.log('You`ve won this round!');
-        }
-    
-    }  
-
-    const playerSelection = prompt('¡Choose Rock, Paper or Scissors!').toLocaleLowerCase(); 
-    const computerSelection = getComputerChoice();
-
-    playRound(playerSelection, computerSelection)
-    }
-}
-
-
-do {
-    if (playerScore === computerScore) {
-        return messageTie;
-    } else if (playerScore < computerScore) {
-        return messageLost; 
-    } else if (playerScore > computerScore) {
-        return messageWin;
-    }
-} while (rounds < 5) {} 
-*/
-
 let rounds = 0;
 
 function game() {
@@ -105,9 +31,6 @@ function game() {
 
 
     function playRound(playerSelection, computerSelection) {
-      /*  if (playerSelection != 'rock' || 'paper' || 'scissors') {
-            return
-        }*/
         if (playerSelection === computerSelection) {
             playerScore++
             computerSelection++
@@ -135,7 +58,7 @@ function game() {
 
     }  
 
-    const playerSelection = prompt('¡Choose Rock, Paper or Scissors!').toLocaleLowerCase(); 
+    const playerSelection = prompt('¡Choose Rock, Paper or Scissors!').toLowerCase(); 
     const computerSelection = getComputerChoice();
 
     function scoreComparison(playerScore, computerScore) {
